db514b16e98a38e123e550f53bac7d087a53cf70
DenisV <volchekdenisminsk@gmail.com>
2024-01-26 13:26:05 +0700

    chore: remove conditional add item logic from Card item

--- apps/angular/projection/src/app/ui/card/card.component.ts
+++ apps/angular/projection/src/app/ui/card/card.component.ts
@@ -1,6 +1,5 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { randStudent, randTeacher } from '../../data-access/fake-http.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { StudentStore } from '../../data-access/student.store';
 import { TeacherStore } from '../../data-access/teacher.store';
 import { CardType } from '../../model/card.model';
@@ -13,6 +12,8 @@ import { ListItemComponent } from '../list-item/list-item.component';
   imports: [NgIf, NgFor, ListItemComponent],
 })
 export class CardComponent {
+  @Output() add = new EventEmitter<void>();
+
   @Input() list: any[] | null = null;
   @Input() type!: CardType;
   @Input() customClass = '';
@@ -23,14 +24,6 @@ export class CardComponent {
     private teacherStore: TeacherStore,
     private studentStore: StudentStore,
   ) {}
-
-  addNewItem() {
-    if (this.type === CardType.TEACHER) {
-      this.teacherStore.addOne(randTeacher());
-    } else if (this.type === CardType.STUDENT) {
-      this.studentStore.addOne(randStudent());
-    }
-  }
 
   deleteItem(id: number) {
     if (this.type === CardType.TEACHER) {
